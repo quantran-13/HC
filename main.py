bf203923f153d4e3617721140d1267bc87eca5ce
xuantruongdang <xuantruongdang.it@gmail.com>
2021-01-30 11:07:25 +0700

    merge reg & seg

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def parse_args():
 if __name__ == "__main__":
     args = parse_args()
     image_path = args.image_path
-    model_path = args.model_path
+    # model_path = args.model_path
 
     if args.method == 'r':
         infer_reg.show_pred(image_path)
